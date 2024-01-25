03ba32beee71b0ee02123a69f7e3c152dd39adb9
Tom <tom.dardilhac@laposte.net>
2024-01-25 16:11:15 +0100

    Création du backend

--- vue_app_frontend/src/router/index.js
+++ vue_app_frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import FeedView from '../views/FeedView.vue'
+import SearchView from '../views/SearchView.vue'
+import MessagesView from '../views/MessagesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,13 +25,20 @@ const router = createRouter({
       component: RegisterView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/feed',
+      name: 'feed',
+      component: FeedView
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: MessagesView
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: SearchView
+    },
   ]
 })
 
--- vue_app_frontend/src/views/ChatView.vue
+++ vue_app_frontend/src/views/ChatView.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
         <div class="main-left col-span-1">
-
+            
         </div>
 
-            <div class="main-center col-span-3 space-y-4">
-                <div class="bg-white border border-gray-200 rounded-lg">
-                    <div class="flex flex-col flex-grow p-4">
+        <div class="main-center col-span-3 space-y-4">
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <div class="flex flex-col flex-grow p-4">
                     
+                </div>
+            </div>
+
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <form>
+                    <div class="p-4">  
+                        <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What do you want to say?"></textarea>
                     </div>
-                </div>
 
-                <div class="bg-white border border-gray-200 rounded-lg">
-                    <form>
-                        <div class="p-4">  
-                            <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What do you want to say?"></textarea>
-                        </div>
-
-                        <div class="p-4 border-t border-gray-100 flex justify-between">
-                            <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Send</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="p-4 border-t border-gray-100 flex justify-between">
+                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Send</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+    
 </template>
